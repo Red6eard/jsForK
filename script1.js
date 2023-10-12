b3e841a93688d80e398ae1e1c2fe3e910fff8fed
Red6eard <vadim.71rus@gmail.com>
2023-10-12 13:44:42 +0300

    first commit

---
+++ script1.js
@@ -0,0 +1,16 @@
+const secondsInAMinute = 60;
+const minutesInAHourse = 60;
+let secondsInAHour = secondsInAMinute * minutesInAHourse;
+const hoursInADay = 24;
+let secondsInADay = secondsInAHour * hoursInADay;
+const dayInAYear = 365;
+let secondsInAYear = secondsInADay * dayInAYear;
+
+let myAge = 37;
+
+
+const secondsInMyAge = (age) => {
+  return age * secondsInAYear
+};
+
+console.log(secondsInMyAge(myAge));
---
+++ script2.js
@@ -0,0 +1,8 @@
+let myString = "heLlo woRlD";
+
+const normalizedString = (string) => {
+  
+}
+
+
+console.log(myString);
