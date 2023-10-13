b41e675d763ab7b96864b029bb44b446c1281dee
Red6eard <vadim.71rus@gmail.com>
2023-10-13 19:08:07 +0300

    небольшие правки предыдушего коммита

--- script4_1.js
+++ script4_1.js
@@ -1,6 +1,6 @@
-let bodyPartsArr = ["нос","глаз","ухо","рука"];
-let adjectiveArr = ["вонючая","глупая","дурацкая"];
-let wordsArr = ["мочалка","дубина","репа","дверь","лопата"];
+let bodyPartsArr = ["попа","голова","нос","глаз","ухо","рука"];
+let adjectiveArr = ["вонючая","глупая","дурацкая","синяя"];
+let wordsArr = ["мочалка","дубина","репа","дверь","лопата","тыква","лужа"];
 
 const getWord = (arr) => {
   return arr[Math.floor(Math.random()*arr.length)];
