f4071169c9ed217ad6dd0159410e7811f327bd72
Red6eard <vadim.71rus@gmail.com>
2023-10-13 18:29:25 +0300

    работа с массивом#2

--- script4.js
+++ script4.js
@@ -6,4 +6,13 @@ const getRandomInsult = (firstWord,secondWord,thirdWord) => {
   return `У тебя ${firstWord[Math.floor(Math.random()*firstWord.length)]} как ${secondWord[Math.floor(Math.random()*secondWord.length)]} ${thirdWord[Math.floor(Math.random()*thirdWord.length)]}!!!`
 };
 
-console.log(getRandomInsult(bodyParts,adjective,word));
+//console.log(getRandomInsult(bodyParts,adjective,word));
+
+// задание 2
+
+const arrayOfNumber = [3,2,1];
+
+const joinToString = (arr) => {
+  return arr.join(" больше, чем ")
+};
+console.log(joinToString(arrayOfNumber), getRandomInsult(bodyParts,adjective,word));
