b93969fc5c3168d696238e75ade0800873972075
Red6eard <vadim.71rus@gmail.com>
2023-10-13 15:22:03 +0300

    функция нормализации регистра строки

--- script2.js
+++ script2.js
@@ -1,8 +1,8 @@
 let myString = "heLlo woRlD";
 
 const normalizedString = (string) => {
-  
-}
+  return myString.slice(0,1).toUpperCase() + myString.slice(1).toLowerCase();
+};
 
 
-console.log(myString);
+console.log(normalizedString(myString));
