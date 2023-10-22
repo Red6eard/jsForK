a8a38f67b9ccf236e21cd855775978c2cff1a831
vadim.71rus@gmail.com <vadim.71rus@gmail.com>
2023-10-22 21:45:21 +0300

    добавлены попытки

--- wordGame/wordScript.js
+++ wordGame/wordScript.js
@@ -18,13 +18,13 @@ for (let i = 0; i < word.length; i++) {
 
 // создаем переменную для отслеживания кол-ва букв которые осталось отгадать
 let remainingLeters = word.length;
-
+let remainingAttempts = 10;
 // игровой цикл
-while (remainingLeters > 0 ) {
+while (remainingLeters > 0 && remainingAttempts > 0) {
     //показываем состояние игры
     alert(answerArray.join(" "));
     //запрашиваем вариант ответа
-    let guess = prompt("Угадай букву или нажми 'Отмена' для выхода из игры").toLowerCase();
+    let guess = prompt("Угадай букву или нажми 'Отмена' для выхода из игры");
     if (guess === null) {
     //если ответ не введен выходим из игрового цикла
     break;
@@ -32,17 +32,28 @@ while (remainingLeters > 0 ) {
         //если введеный ответ НЕ равен одному символу
         alert("Введите только ОДНУ букву!");
     } else {
-        //если все верно, обновляем состояние игры
+        // если все верно, обновляем состояние игры
+        let foundLetter = false;
         for (let i = 0; i < word.length; i++) {
-            if (word[i] === guess) {
+            if (word[i].toLowerCase() === guess.toLowerCase()) {
                 answerArray[i] = guess;
                 remainingLeters--;
+                foundLetter = true;
             }
         }
+  
+        if (!foundLetter) {
+            // если буква не была найдена, уменьшаем количество попыток
+            remainingAttempts--;
+            alert(`Неправильная буква! Осталось ${remainingAttempts} попыток.`);
+        }
     }
-    //конец игрового цикла
+// конец игрового цикла
 };
-
-//отображаем ответ и поздравляем игрока
-alert(answerArray.join(" "));
-alert(`Поздравляем, ты отгадал слово: ${word.toUpperCase()}!!!`)
+    //отображаем ответ и поздравляем игрока
+    alert(answerArray.join(" "));
+    if (remainingLeters === 0) {
+      alert(`Поздравляем, ты отгадал слово: ${word.toUpperCase()}!!!`);
+    } else {
+      alert("Упс! Ты не справился. Попробуй еще раз!");
+    }
