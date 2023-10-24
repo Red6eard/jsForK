66fb14542fa7e3eb569368b4f589ba7785402a9a
vadim.71rus@gmail.com <vadim.71rus@gmail.com>
2023-10-24 14:10:25 +0300

    переписана функция на генераторе дразнилок

--- script4.js
+++ script4.js
@@ -3,13 +3,20 @@ let bodyParts = ["нос","глаз","ухо","рука"];
 let adjective = ["вонючая","глупая","дурацкая"];
 let word = ["мочалка","дубина","репа","дверь","лопата"];
 
+// функция получения рандомного слова
+const getRandomValue = (arr) => {
+  return arr[Math.floor(Math.random() * arr.length)]
+};
+// функция получения дразнилки из разных слов
+const getRandomInsult = () => {
+  return `У тебя ${getRandomValue(bodyParts)} как ${getRandomValue(adjective)} ${getRandomValue(word)}!!!`;
+}
 
-
-const getRandomInsult = (firstWord,secondWord,thirdWord) => {
+/*const getRandomInsult = (firstWord,secondWord,thirdWord) => {
   return `У тебя ${firstWord[Math.floor(Math.random()*firstWord.length)]} как ${secondWord[Math.floor(Math.random()*secondWord.length)]} ${thirdWord[Math.floor(Math.random()*thirdWord.length)]}!!!`
 };
 
-//console.log(getRandomInsult(bodyParts,adjective,word));
+console.log(getRandomInsult(bodyParts,adjective,word));*/
 
 // задание 2
 
