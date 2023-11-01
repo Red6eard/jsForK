932d2f7de2c8d18b1fd0ec79b11d3b9fbeac0b57
vadim.71rus@gmail.com <vadim.71rus@gmail.com>
2023-11-01 18:02:29 +0300

    переписано с ошибками. дебаг нужен

--- script7_1.js
+++ script7_1.js
@@ -1,17 +1,13 @@
 
 let arraySome1 = [1, 2, 3];
-let arraySome2 = [,2,3];
+let arraySome2 = [1,2,3];
 
 const areArraysSame = (arr1, arr2) => {
   if (arr1.length !== arr2.length){
     return false
   };
   for (let i = 0; i < arr1.length; i++){
-    if (arr1[i] === arr2[i]){
-      return true
-    } else {
-      return false
-    }
+    return arr1[i] === arr2[i]
   }
 };
 
