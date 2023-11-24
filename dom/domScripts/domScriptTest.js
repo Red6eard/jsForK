40c1aeb458e37a42825efd193825d7aa5d8764bf
vadim.71rus@gmail.com <vadim.71rus@gmail.com>
2023-11-24 19:35:53 +0300

    update domScriptTest.js

--- dom/domScripts/domScriptTest.js
+++ dom/domScripts/domScriptTest.js
@@ -1,10 +1,15 @@
 $("body").append("<h1 id='head'>Видимый заголовок</h1>");
 //let head = document.getElementById('head');
 //head.innerHTML = 'New heading';
-$('#head').fadeOut(2000);
+$('#head').fadeOut(2000)
 $("body").append("<p id='para'>Первый параграф</p>");
 
 $("body").append("<p id='para2'>Второй параграф</p>");
 
-
 $("body").append("<p id='para3'>Третий параграф</p>");
+$("#para").slideUp(2000);
+$("#para").slideDown(2000);
+$("#head").fadeIn(3000);
+$("#para2").hide();
+$("#para2").fadeIn(3000);
+$("#para3").fadeTo(3000,0.1)
