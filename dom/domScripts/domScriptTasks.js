5bb9a0ff996843be27790281c0a8cb0960fb8d03
vadim.71rus@gmail.com <vadim.71rus@gmail.com>
2023-11-26 13:39:47 +0300

    правки в решениях

--- dom/domScripts/domScriptTasks.js
+++ dom/domScripts/domScriptTasks.js
@@ -3,17 +3,17 @@ let friends = ['Alex','Mikey','Dan'];
 
 $('body').append('<h1>My friends</h1>');
 for(let i=0; i<friends.length; i++){
-  $('body').append(`<p>${friends[i]} the best!!!</p>`)
+  $('body').append(`<p>${friends[i]}</p>`)
 };
-$('p').hide().fadeIn(3000);
+$('p').append('<span> the best!!!</span>')
+$('span').hide().fadeIn(5000);
 
 // task2
 let num = 1000;
 for(let i=0; i<10; i++){
-  $('h1').fadeOut(num);
-  $('h1').delay(5000).fadeIn(num);
+  $('h1').delay(5000).fadeOut(num);
+  $('h1').fadeIn(num);
   num += 1000;
 };
 
-//task 3
 
